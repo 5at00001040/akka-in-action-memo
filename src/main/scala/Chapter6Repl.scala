262c182766700073019944ebffd55cfcb18178c8
Sato <5at0000.10.40@gmail.com>
2018-04-18 23:21:52 +0900

    chapter 6 code

---
+++ src/main/scala/Chapter6Repl.scala
@@ -0,0 +1,96 @@
+object BackendRepl {
+
+  // start sbt console
+  // sbt console
+
+  // list 6.2
+
+  val conf = """
+  akka {
+    actor {
+      provider = "akka.remote.RemoteActorRefProvider"
+    }
+    remote {
+      enabled-transports = ["akka.remote.netty.tcp"]
+      netty.tcp {
+        hostname = "0.0.0.0"
+        port = 2551
+      }
+    }
+  }
+  """
+
+  // list 6.3
+
+  import com.typesafe.config._
+  import akka.actor._
+
+  // start akka
+  val config = ConfigFactory.parseString(conf)
+  val backend = ActorSystem("backend", config)
+
+
+  // list 6.4
+
+  class Simple extends Actor {
+    def receive = {
+      case m => println(s"received $m!")
+    }
+  }
+
+  backend.actorOf(Props[Simple], "simple")
+
+  // stop akka
+  // backend.terminate()
+
+  // quit sbt console
+  // :q
+
+}
+
+object FrontendRepl {
+
+  // start sbt console
+  // sbt console
+
+  // list 6.5
+
+  val conf = """
+    akka {
+    actor {
+      provider = "akka.remote.RemoteActorRefProvider"
+    }
+    remote {
+      enabled-transports = ["akka.remote.netty.tcp"]
+      netty.tcp {
+        hostname = "0.0.0.0"
+        port = 2552
+      }
+    }
+  }
+  """
+
+  import com.typesafe.config._
+  import akka.actor._
+
+  // start akka
+  val config = ConfigFactory.parseString(conf)
+  val frontend = ActorSystem("frontend", config)
+
+
+
+  // list 6.6
+
+  val path = "akka.tcp://backend@0.0.0.0:2551/user/simple"
+  val simple = frontend.actorSelection(path)
+
+  simple ! "Hello Remote World!"
+
+
+  // stop akka
+  // frontend.terminate()
+
+  // quit sbt console
+  // :q
+
+}
