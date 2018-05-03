91a4c6a087aa3735eba5ccebc0349ea7254b23cd
Sato <5at0000.10.40@gmail.com>
2018-05-03 12:44:58 +0900

    chapter9 hash router

---
+++ src/test/scala/chapter9/HashRouterTest.scala
@@ -0,0 +1,61 @@
+package chapter9
+
+import scala.concurrent.duration._
+
+import akka.actor._
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import akka.routing._
+import akka.routing.ConsistentHashingRouter._
+
+import akka.testkit.{TestProbe, TestKit}
+
+class HashRouterTest
+  extends TestKit(ActorSystem("PerfRoutingTest"))
+    with WordSpecLike with BeforeAndAfterAll {
+
+  override def afterAll() = {
+    system.terminate()
+  }
+
+  "The HashRouting" must {
+    "work using mapping" in {
+      val endProbe = TestProbe()
+
+      def hashMapping: ConsistentHashMapping = {
+        case msg: GatherMessage => msg.id
+      }
+
+      val router = system.actorOf(ConsistentHashingPool(10, virtualNodesFactor = 10, hashMapping = hashMapping).
+        props(Props(new SimpleGather(endProbe.ref))))
+
+      router ! GatherMessageNormalImpl("1", Seq("msg1"))
+      endProbe.expectNoMsg(100.millis)
+      router ! GatherMessageNormalImpl("1", Seq("msg2"))
+      endProbe.expectMsg(GatherMessageNormalImpl("1",Seq("msg1","msg2")))
+
+      router ! GatherMessageNormalImpl("10", Seq("msg1"))
+      endProbe.expectNoMsg(100.millis)
+      router ! GatherMessageNormalImpl("10", Seq("msg2"))
+      endProbe.expectMsg(GatherMessageNormalImpl("10",Seq("msg1","msg2")))
+      system.stop(router)
+    }
+    "work using messages" in {
+      val endProbe = TestProbe()
+
+      val router = system.actorOf(ConsistentHashingPool(10, virtualNodesFactor = 10).
+        props(Props(new SimpleGather(endProbe.ref))))
+
+      router ! GatherMessageWithHash("1", Seq("msg1"))
+      endProbe.expectNoMsg(100.millis)
+      router ! GatherMessageWithHash("1", Seq("msg2"))
+      endProbe.expectMsg(GatherMessageNormalImpl("1",Seq("msg1","msg2")))
+
+      router ! GatherMessageWithHash("10", Seq("msg1"))
+      endProbe.expectNoMsg(100.millis)
+      router ! GatherMessageWithHash("10", Seq("msg2"))
+      endProbe.expectMsg(GatherMessageNormalImpl("10",Seq("msg1","msg2")))
+      system.stop(router)
+    }
+  }
+
+}
