8c099aae1f3ccc3c6a6c4d14d5fdefe0e10870c3
Sato <5at0000.10.40@gmail.com>
2018-05-03 13:38:24 +0900

    chapter9 switch router

---
+++ src/main/scala/chapter9/SwitchRouter.scala
@@ -0,0 +1,31 @@
+package chapter9
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+
+case class RouteStateOn()
+case class RouteStateOff()
+
+class SwitchRouter(normalFlow: ActorRef, cleanUp: ActorRef)
+  extends Actor with ActorLogging {
+
+  def on: Receive = {
+    case RouteStateOn =>
+      log.warning("Received on while already in on state")
+    case RouteStateOff => context.become(off)
+    case msg: AnyRef => {
+      normalFlow ! msg
+    }
+  }
+  def off: Receive = {
+    case RouteStateOn => context.become(on)
+    case RouteStateOff =>
+      log.warning("Received off while already in off state")
+    case msg: AnyRef => {
+      cleanUp ! msg
+    }
+  }
+  def receive = {
+    case msg: AnyRef => off(msg)
+  }
+}
+
