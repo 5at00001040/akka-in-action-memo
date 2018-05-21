434754155d420b75705f9496e1d955e505a5fcf6
Sato <5at0000.10.40@gmail.com>
2018-05-21 22:15:41 +0900

    chapter12 unmarshaller marshaller

---
+++ src/main/scala/chapter12/EventUnmarshaller.scala
@@ -0,0 +1,37 @@
+package chapter12
+
+import akka.http.scaladsl.model.{ContentTypeRange, ContentTypes, HttpEntity}
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.http.scaladsl.unmarshalling.Unmarshaller._
+import akka.http.scaladsl.unmarshalling.Unmarshaller.UnsupportedContentTypeException
+import akka.stream.Materializer
+import akka.stream.scaladsl.Source
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+
+
+object EventUnmarshaller extends EventMarshalling {
+  val supported = Set[ContentTypeRange](
+    ContentTypes.`text/plain(UTF-8)`,
+    ContentTypes.`application/json`
+  )
+
+  def create(maxLine: Int, maxJsonObject: Int) = {
+    new Unmarshaller[HttpEntity, Source[Event, _]] {
+      def apply(entity: HttpEntity)(implicit ec: ExecutionContext,
+                                    materializer: Materializer): Future[Source[Event, _]] = {
+
+        val future = entity.contentType match {
+          case ContentTypes.`text/plain(UTF-8)` => Future.successful(LogJson.textInFlow(maxLine))
+          case ContentTypes.`application/json` => Future.successful(LogJson.jsonInFlow(maxJsonObject))
+          case other => Future.failed(new UnsupportedContentTypeException(supported))
+        }
+
+        future.map(flow => entity.dataBytes.via(flow))(ec)
+      }
+    }.forContentTypes(supported.toList:_*)
+  }
+}
+
--- src/main/scala/chapter12/LogsApi.scala
+++ src/main/scala/chapter12/LogsApi.scala
@@ -8,18 +8,23 @@ import scala.util.{Failure, Success}
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Route
 import akka.stream.{ActorMaterializer, IOResult}
-import akka.stream.scaladsl.{BidiFlow, FileIO, Flow, Framing, Keep}
+import akka.stream.scaladsl.{BidiFlow, FileIO, Flow, Framing, Keep, Source}
 import akka.util.ByteString
 
 import scala.concurrent.ExecutionContext
 import spray.json._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.marshalling.Marshal
 
 
 // test command
 /*
 echo -e "my-host-1 | web-app | ok    | 2018-05-09T15:30:01.127Z | 5 tickets sold. ||\n" | http -v POST http://localhost:5000/logs/1 Content-Type:text/plain
 http -v GET http://localhost:5000/logs/1
+
+http -v POST http://localhost:5000/any-content-logs/2 Content-Type:text/plain < xxx.txt
+http -v POST http://localhost:5000/any-content-logs/3 Content-Type:application/json < yyy.json
  */
 
 class LogsApi(
@@ -55,7 +60,7 @@ class LogsApi(
   def logFileSink(logId: String) = FileIO.toPath(logFile(logId), Set(CREATE, WRITE, APPEND))
   def logFileSource(logId: String) = FileIO.fromPath(logFile(logId))
 
-  def routes: Route = postRoute ~ getRoute
+  def routes: Route = postRoute ~ postMultiTypeRoute ~ getRoute ~ getMultiTypeRoute
 
   def postRoute =
     pathPrefix("logs" / Segment) { logId =>
@@ -66,6 +71,33 @@ class LogsApi(
               entity
                 .dataBytes
                 .via(logToJsonFlow)
+                .toMat(logFileSink(logId))(Keep.right)
+                .run()
+            ) {
+              case Success(IOResult(count, Success(Done))) =>
+                complete((StatusCodes.OK, LogReceipt(logId, count)))
+              case Success(IOResult(count, Failure(e))) =>
+                complete((StatusCodes.BadRequest, ParseError(logId, e.getMessage)))
+              case Failure(e) =>
+                complete((StatusCodes.BadRequest, ParseError(logId, e.getMessage)))
+            }
+          }
+        }
+      }
+    }
+
+
+  // echo -e "my-host-1 | web-app | ok    | 2018-05-09T15:30:01.127Z | 5 tickets sold. ||\n" | http -v POST http://localhost:5000/multi-type-logs/2 Content-Type:"text/plain; charset=UTF-8"
+  // cat logs/2 | http -v POST http://localhost:5000/multi-type-logs/3 Content-Type:application/json
+  val maxJsonObject = 102400
+  implicit val unmarshaller = EventUnmarshaller.create(maxLine, maxJsonObject)
+  def postMultiTypeRoute =
+    pathPrefix("multi-type-logs" / Segment) { logId =>
+      pathEndOrSingleSlash {
+        post {
+          entity(as[Source[Event, _]]) { src =>
+            onComplete(
+              src.via(outFlow)
                 .toMat(logFileSink(logId))(Keep.right)
                 .run()
             ) {
@@ -96,5 +128,23 @@ class LogsApi(
         }
       }
 
+    }
+
+  // http -v GET http://localhost:5000/multi-type-logs/3 Content-Type:application/json
+  implicit val marshaller = LogEntityMarshaller.create(maxJsonObject)
+  def getMultiTypeRoute =
+    pathPrefix("multi-type-logs" / Segment) { logId =>
+      pathEndOrSingleSlash {
+        get {
+          extractRequest { req =>
+            if (Files.exists(logFile(logId))) {
+              val src = logFileSource(logId)
+              complete(Marshal(src).toResponseFor(req))
+            } else {
+              complete(StatusCodes.NotFound)
+            }
+          }
+        }
+      }
     }
 }
